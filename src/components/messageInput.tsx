06766851857e557addbdd10717ea4eceb4834961
Yan Castro <yancastrohenrique1@gmail.com>
2023-12-05 18:57:02 -0300

    fix: enter message input

--- src/components/messageInput.tsx
+++ src/components/messageInput.tsx
@@ -8,12 +8,20 @@ const MessageInput = () => {
   const [value, setValue] = useState("");
   const { send } = useSocket();
 
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      send(value);
+    }
+  };
+
   return (
     <div className="grid w-full mx-10 mb-5 gap-2">
       <Textarea
         placeholder="Escreva a sua mensagem"
         value={value}
         onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <Button onClick={() => send(value)}>Enviar mensagem</Button>
     </div>
